3fea138420487fa48ba8d35b5919b9ee3f3a2b4e
Dylan Moore <hosnappp@gmail.com>
2016-10-05 21:13:11 -0400

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -17,7 +17,7 @@ while True:
         scaleFactor=1.2,
         minNeighbors=5,
         minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        flags=cv2.CV_HAAR_SCALE_IMAGE
     )
 
     # Draw a rectangle around the faces
